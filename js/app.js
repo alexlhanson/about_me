49e5cd7a67966a5545e3d04ddc90f45e6055988b
Alex Hanson <alexlhanson@gmail.com>
2018-06-12 13:50:36 -0700

    Added an introduction to the JS questions

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+//welcome message
+alert ('Hello and welcome! Let\'s get to know the nitty gritties of Alex');
+
+//prompt for a username
+var username = prompt('But to begin, what is your name?');
+console.log('What is your name? ; ' + username);
+alert ('Hi, nice to meet you ' + username);
+
